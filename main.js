1526ddafcd5ebf28e5aab54d6a357ed1d1df16fe
Randhir200 <mail.randhirkr@gmail.com>
2022-05-14 17:54:40 +0530

    Brtue Force Approach

---
+++ main.js
@@ -0,0 +1,23 @@
+var reversePrefix = function(word, ch) {
+    let ind;
+    for(i=0;i<word.length;i++){
+        if(ch == word[i]){
+            ind = i;
+            break;
+        }
+    }
+    let bag = "";
+    for(i=ind;i>=0;i--){
+       bag+=word[i] 
+    }
+    
+     for(i=ind+1;i<word.length;i++){
+       bag+=word[i] 
+    }
+    if(!ind){
+        return word;
+    }
+    return bag;
+};
+
+console.log(reversePrefix("abcdefd"))
